edc5ad8e4c3a3eb3bb14acf5f56f5241f398e8a3
shivamm141 <shivammore1412006@gmail.com>
2026-07-22 12:12:13 +0530

    Integrate openweather API

--- src/main/java/com/weatherapp/weatherapp/service/WeatherService.java
+++ src/main/java/com/weatherapp/weatherapp/service/WeatherService.java
@@ -1,11 +1,30 @@
 package com.weatherapp.weatherapp.service;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
 
 @Service
 public class WeatherService {
+
+    @Value("${weather.api.key}")//find the configration property called weather,api,key and put its value into tis variable
+    private String apiKey;
+
+    private final RestClient restClient; // used to send http request
+    public WeatherService() {
+    this.restClient = RestClient.create();
+    }
+
     public String getWeather(String city){
-        return "weather data from service for" + city;
+
+        String response = restClient.get() //use our resclient object to prepare an http get request
+        .uri ("https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}",
+                    city, apiKey)
+        .retrieve()//senf the request and prepare for the reponse
+        .body(String.class); // whateer maybe the respone give it in string
+        return response;
 
     }
 }
